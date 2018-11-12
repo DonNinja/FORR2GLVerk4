b60391cfdf857f6143a8d61ff3524f561436bc0a
DonNinja <yngvileo@gmail.com>
2018-11-12 11:11:17 +0000

    first commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform Player;
+
+    public Vector3 cameraOffset;
+    
+    private readonly float smoothFactor = 1f;
+
+    public float rotationSpeedHor = 5f;
+    //public float rotationSpeedVert = 5f;
+
+    // Use this for initialization
+    void Start () {
+        cameraOffset = transform.position - Player.position;
+	}
+
+    void LateUpdate () {
+
+        Quaternion camTurnAngleHor = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeedHor, Vector3.up);
+        cameraOffset = camTurnAngleHor * cameraOffset;
+
+        //Quaternion camTurnAngleVert = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotationSpeedVert, Vector3.left);
+        //cameraOffset = camTurnAngleVert * cameraOffset;
+
+        Vector3 newPos = Player.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        transform.LookAt(Player);
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float timeBetweenShots = 2f;
+    float timer;
+    public Rigidbody playerball;
+    public float playerSpeed;
+    public float force;
+    Vector3 lastposition = new Vector3();
+    private bool finished;
+    //public Camera main;
+
+    private void Start()
+    {
+        Physics.sleepThreshold = 0.1f;
+    }
+
+    private void Update()
+    {
+        if (finished)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        timer += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (playerSpeed == 0 || timer >= timeBetweenShots && Time.timeScale != 0)
+            {
+                playerball.AddForce(Camera.main.transform.forward * force);
+            }
+        }
+        playerSpeed = (transform.position - lastposition).magnitude;
+        lastposition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider finish)
+    {
+        if (finish.gameObject.tag == "Finish")
+        {
+            finished = true;
+        }
+    }
+}
